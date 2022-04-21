6d87364f8779bb381de9cce65be29160c9f3b9c7
Daniel Cui <solunian@gmail.com>
2022-04-20 17:21:05 -0700

    Complete basic storage functionality
    Blocked urls can now be kept in storage and persist
    even after reloading. Deleting still not implemented but
    should work on background tab detection before adding more.

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,78 +2,61 @@ const urlBox = document.querySelector("#urlBox-input");
 const enterButton = document.querySelector("#urlInputEnter-button");
 const blockedUrlUl = document.querySelector("#blockedUrls-ul");
 
-// callbacks
 
-// Where we will expose all the data we retrieve from storage.sync.
-const storageCache = {};
-// Asynchronously retrieve data from storage.sync, then cache it.
-const initStorageCache = getAllStorageSyncData().then(items => {
-    // Copy the data retrieved from storage into storageCache.
-    Object.assign(storageCache, items);
-});
-
-chrome.action.onClicked.addListener(async (tab) => {
-    try {
-        await initStorageCache;
-    } catch (e) {
-        // Handle error that occurred during storage initialization.
-    }
-    // Normal action handler logic.
-});
-
-// Reads all data out of storage.sync and exposes it via a promise.
-//
-// Note: Once the Storage API gains promise support, this function
-// can be greatly simplified.
-function getAllStorageSyncData() {
-    // Immediately return a promise and start asynchronous work
-    return new Promise((resolve, reject) => {
-        // Asynchronously fetch all data from storage.sync.
-        chrome.storage.sync.get(null, (items) => {
-            // Pass any observed errors down the promise chain.
-            if (chrome.runtime.lastError) {
-                return reject(chrome.runtime.lastError);
-            }
-            // Pass the data retrieved from storage down the promise chain.
-            resolve(items);
-        });
-    });
-}
-
-
-function addBlockedUrl(url) {
-    urlBox.value = null;
-    urlListElement = document.createElement("li");
-    urlListElement.innerText = url;
-    blockedUrlUl.appendChild(urlListElement);
-
+// clears blockedUrlUl of items and updates it from storage
+function updateBlockedUrlUl() {
     chrome.storage.sync.get("blocked_urls").then(val => {
-        if (typeof val == undefined) {
-            const blocked_urls = [url];
-            chrome.storage.sync.set({"blocked_urls": blocked_urls});
-            console.log("storage is undefined");
-        } else {
-            const blocked_urls = storageCache["blocked_urls"] ?? [];
-            blocked_urls.push(url);
-            chrome.storage.sync.set({"blocked_urls": blocked_urls});
-            console.log("storage is defined");
+        blockedUrlUl.innerHTML = null;
+        if (Object.values(val).length == 0) {
+            return;
         }
-    });
-    
+        for (const url of val["blocked_urls"]) {
+            urlBox.value = null;
+            urlListElement = document.createElement("li");
+            urlListElement.innerText = url;
+            blockedUrlUl.appendChild(urlListElement);
+        }
+    }); 
+}
 
+function addBlockedUrlToStorage(url) {
     chrome.storage.sync.get("blocked_urls").then(val => {
-        console.log(val);
+        let blocked_urls = [];
+        if (Object.values(val).length > 0) {
+            blocked_urls = val["blocked_urls"];
+        }
+        blocked_urls.push(url);
+        chrome.storage.sync.set({"blocked_urls": blocked_urls});
+    }).catch(e => {
+        throw e;
     });
 }
 
+function addNewBlockedUrl(url) {
+    addBlockedUrlToStorage(url);
+    addBlockedUrlToDOM(url);
+}
 
-// Event Listeners
+document.addEventListener("DOMContentLoaded", e => {
+    // console.log("dom loaded!");
+    updateBlockedUrlUl();
+    
+});
+
+chrome.storage.onChanged.addListener(() => {
+    // console.log("data changed!");
+    updateBlockedUrlUl();
+})
+
+
+// DOM Listeners
+
 urlBox.addEventListener("keydown", e => {
     if (e.key === "Enter") {
-        addBlockedUrl(urlBox.value);
+        addBlockedUrlToStorage(urlBox.value);
     }
 });
 
 enterButton.addEventListener("click", () => {
-    addBlockedUrl(urlBox.value);
+    addNewBlockedUrlToStorage(urlBox.value);
 });
